dc56f47628bc5aef58342908e0777d4f565b432a
Даниил Балабанов <romheldan99@gmail.com>
2021-03-11 15:09:54 +0300

    +Economy , clear

---
+++ Economy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using OxyPlot;
+
+namespace DiplomaLol
+{
+    public partial class Game
+    {
+        private void Economy()
+        {
+            var rand = new Random();    // Создание экземпляра класса Random для получения функция рандома
+            Mod = rand.Next(-1 * difficulty, 1 * difficulty + 1); // Присвоение Модификатору случайного значения из диапазона
+
+            while (Mod == ModPrevious)  // Пока в предыдущей неделе был такой же модификатор
+            {
+                Mod = rand.Next(-1 * difficulty, 1 * difficulty + 1); // Реролл
+            }
+
+            Price += Mod;   // Меняем цену на новую
+            ModPrevious = Mod;  // Перезаписываем Модификатор для следующего цикла
+
+
+            if (weeks % 8 == 0)
+            {
+                Price += 2;
+            }
+
+            if (weeks % 16 == 0)
+            {
+                Price /= 2;
+            }
+
+            if (Price <= 0)  // Проверка на отрицательную стоимость
+            {
+                Price = 1;
+                ModPrevious = 1;
+            }
+
+            MaximumWeeksAxis++;
+            dataPoints.Add(new DataPoint(weeks, Price));
+
+            if (Cash < -1000)   // Условие проигрыша
+            {
+                sw.Stop();
+                MessageBox.Show("Вы проиграли.");
+            }
+
+            if (Cash >= 8000) // Условие победы
+            {
+                sw.Stop();
+                MessageBox.Show("Поздравляю. Вы справились с задачей.");
+            }
+        }
+    }
+}
--- Game.xaml.cs
+++ Game.xaml.cs
@@ -12,9 +12,10 @@ using DiplomaLol.Annotations;
 
 namespace DiplomaLol {
     public partial class Game : Page, INotifyPropertyChanged {
+
         private readonly DispatcherTimer dt = new DispatcherTimer();
         private readonly Stopwatch sw = new Stopwatch();
-        private string currentTime;
+        private string currentTime = "0 weeks 0 days";
         private int days;
         private int weeks;
 
@@ -42,77 +43,76 @@ namespace DiplomaLol {
                 OnPropertyChanged();
             }
         }
-
-
         #endregion
 
 
-        int AmountOfProducts = 5;   // Количество товаров
+        int AmountOfProducts = 5;   // Количество товаров изначально
         int Cash = 2000;    // Баланс
         int SizeOfStorage = 10; // Размер хранилища
         int Rent = 100; // Плата за аренду
         int Mod;    // Модификатор цены
         int ModPrevious;    // Модификатор цены предыдущей недели для псевдорандома
-
         private int Price = 4;
 
         public Game(int difficulty) {
+
             InitializeComponent();
+            
             DataContext = this;
             this.difficulty = difficulty;
-            currentTime = "0 weeks 0 days";
             dt.Tick += new EventHandler(Dt_Tick);
             dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            
+            Update();
 
-            clocktxtblock.Text = "0 weeks 0 days";
-            cashtxtblock.Text = $"$ {Cash}";
-            progressBar.Minimum = 0;                // Иллюстрация
-            progressBar.Maximum = SizeOfStorage;    // Наполненности
-            progressBar.Value = AmountOfProducts;   // Склада
-            storageinfo.Text = $"Products storaged: {AmountOfProducts} \nSize of Storage: {SizeOfStorage}.";
-            cashinfo.Text = $"Rent: {Rent}; Price: {Price}.";
-
-            DataPoints = new ObservableCollection<DataPoint> {
+            DataPoints = new ObservableCollection<DataPoint> {  // Коллекция точек для графика
                 new DataPoint(0, 4)
             };
         }
         void Dt_Tick(object sender, EventArgs e) {
+
             if (sw.IsRunning)   // Пока работает секундомер
             {
                 TimeSpan ts = sw.Elapsed;
                 days = ts.Seconds;
-                currentTime = $"{weeks} weeks {days} days";
-                if (ts.Seconds % 7 == 0 && ts.Seconds > 0) // Круг выполнения цикла While
+                currentTime = $"{weeks} weeks {days} days"; // Обновляем изображение времени с каждым тактом
+                
+                if (ts.Seconds % 7 == 0 && ts.Seconds > 0) // Круг выполнения цикла While для временной составляющей
                 {
                     weeks += 1;
                     sw.Restart();
                     EndOfWeek();
                 }
-                cashtxtblock.Text = $"$ {Cash}";
-                clocktxtblock.Text = currentTime;   // Вывод времени на экран
-                storageinfo.Text = $"Products storaged: {AmountOfProducts} \nSize of Storage: {SizeOfStorage}."; //нужно сделать update по клику
-                cashinfo.Text = $"Rent: {Rent}; Price: {Price}."; //нужно сделать update по клику
-                                                                  // привязка нахуй не всралась, я сделал по таймингу миллисекунд
-                progressBar.Minimum = 0;                // Иллюстрация
-                progressBar.Maximum = SizeOfStorage;    // Наполненности
-                progressBar.Value = AmountOfProducts;   // Склада
-
-                slidersell.Maximum = AmountOfProducts;
-                //   plotGraph
-                // MainViewModel.GraphPoints.Add(new DataPoint(days, Price));
+                Update();  
             }
         }
 
-        private void Startbtn_Click(object sender, RoutedEventArgs e) {
+        private void Update()   // Обновление интерфейса на экране
+        {
+            //-----Вывод статистики на среднюю панель-----
+            storageinfo.Text = $"Products storaged: {AmountOfProducts} \nSize of Storage: {SizeOfStorage}."; 
+            cashinfo.Text = $"Rent: {Rent}; Price: {Price*50}.";
+            progressBar.Minimum = 0;                // Иллюстрация
+            progressBar.Maximum = SizeOfStorage;    // Наполненности
+            progressBar.Value = AmountOfProducts;   // Склада
+
+            slidersell.Maximum = AmountOfProducts;  // Максимальная дальность слайдера для продажи
+
+            cashtxtblock.Text = $"$ {Cash}";    // Вывод текущего баланса на нижней панели
+            clocktxtblock.Text = currentTime;   // Вывод текущего времени на нижней панели
+
+        }
+
+        private void Startbtn_Click(object sender, RoutedEventArgs e) { // По нажатию кнопки Старт начинается игровой процесс
             sw.Start();
             dt.Start();
         }
-        private void Stopbtn_Click(object sender, RoutedEventArgs e) {
+        private void Stopbtn_Click(object sender, RoutedEventArgs e) {  // Можно нажать на Паузу, что более корректного обдумывания
             if (sw.IsRunning) {
                 sw.Stop();
             }
         }
-        private void IncreaseSize(object sender, RoutedEventArgs e) {
+        private void IncreaseSize(object sender, RoutedEventArgs e) {   // Увеличение размера склада
             if (SizeOfStorage < 40) {
                 SizeOfStorage += 10;
                 Rent *= 2;
@@ -120,7 +120,7 @@ namespace DiplomaLol {
                 rectanglestorage.Width += 100;
             }
         }
-        private void DecreaseSize(object sender, RoutedEventArgs e) {
+        private void DecreaseSize(object sender, RoutedEventArgs e) {   // Уменьшение размера склада
             if (SizeOfStorage > 10) {
                 SizeOfStorage -= 10;
                 Rent /= 2;
@@ -130,59 +130,23 @@ namespace DiplomaLol {
         }
         private void EndOfWeek() {
             Cash -= Rent;   // Расходы на аренду
-
-            var rand = new Random();    // Создание экземпляра класса Random для получения функция рандома
-            Mod = rand.Next(-1 * difficulty, 1 * difficulty + 1); // Присвоение Модификатору случайного значения из диапазона
-
-            while (Mod == ModPrevious)  // Пока в предыдущей неделе был такой же модификатор
-            {
-                Mod = rand.Next(-1 * difficulty, 1 * difficulty + 1); // Реролл
-            }
-
-            Price += Mod;   // Меняем цену на новую
-            ModPrevious = Mod;  // Перезаписываем Модификатор для следующего цикла
-
-            if (weeks % 8 == 0) {
-                Price += 2;
-            }
-
-            if (weeks % 16 == 0) {
-                Price /= 2;
-            }
-
-            if (Price <= 0)  // Проверка на отрицательную стоимость
-            {
-                Price = 1;
-                ModPrevious = 1;
-            }
-
-            MaximumWeeksAxis++;
-            dataPoints.Add(new DataPoint(weeks, Price));
-
-            if (Cash < -1000)   // Условие проигрыша
-            {
-                sw.Stop();
-                MessageBox.Show("Вы проиграли.");
-            }
-
-            if (Cash >= 8000) // Условие победы
-            {
-                sw.Stop();
-                MessageBox.Show("Поздравляю. Вы справились с задачей.");
-            }
+            Economy();  // Расчет экономики для следующей недели
         }
 
         private void Buy(object sender, RoutedEventArgs e) {
             if (AmountOfProducts + (int)sliderincome.Value > SizeOfStorage) {
+                sw.Stop();
                 MessageBox.Show("Увеличьте размер склада. Не помещается.");
             } else {
                 AmountOfProducts += (int)sliderincome.Value;
                 Cash -= Price * 50 * (int)sliderincome.Value;
+                Update();
             }
         }
         private void Sell(object sender, RoutedEventArgs e) {
             AmountOfProducts -= (int)slidersell.Value;
             Cash += Price * 50 * (int)slidersell.Value;
+            Update();
         }
 
 
